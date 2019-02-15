42032dbc1778e25699682d18c910469a00d5dc82
CRNE <47609964+CRNE@users.noreply.github.com>
2019-02-15 09:00:31 +0000

    Update mockchart.js

--- mockchart.js
+++ mockchart.js
@@ -1,6 +1,6 @@
 // Create mock data
 var firstDataSet = [0, 2, 4, 6, 8, 10, 12, 14];
-var secondDataSet = [0, 1, 2, 3, 4, 5, 6, 7];
+var secondDataSet = [Energy, Infrastructure mining and comodities, transport, Other];
 
 // Create chart
 var myConfig = {
